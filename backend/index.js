f5173258de6630801fc0362ec9f661181012cbee
Himanshu <hv308489@gmail.com>
2025-05-31 23:20:35 +0530

    cors endpoint

--- backend/index.js
+++ backend/index.js
@@ -9,19 +9,26 @@ const stationRoutes = require('./routes/stations');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true 
+})
+);
+
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Could not connect to MongoDB', err));
+  .then(() => console.log('connected to db'))
+  .catch(err => console.error('could not connect to db', err));
 
-app.use('/api/auth', authRoutes);
+  app.get('/', (req,res) => {
+    res.send('server is running');
+  })
+
+  app.use('/api/auth', authRoutes);
 app.use('/api/stations', stationRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Charging Stations API is running');
-});
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
